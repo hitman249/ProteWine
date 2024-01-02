c1cd384ac1c1be6ccd79bb9a2ccbb20290e53847
hitman249 <hitman2491@gmail.com>
2024-01-03 01:53:24 +0700

    icon

--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,24 @@ import {app, BrowserWindow} from 'electron';
 
 const createWindow: () => void = (): void => {
   const win: BrowserWindow = new BrowserWindow({
+    icon: __dirname + '/icons/512.png',
+    minWidth: 800,
+    minHeight: 600,
     width: 800,
     height: 600,
+    webPreferences: {
+      sandbox: true,
+      allowRunningInsecureContent: true,
+      webSecurity: true,
+      nodeIntegration: true,
+      nodeIntegrationInWorker: true,
+      backgroundThrottling: true,
+    },
   });
 
-  win.loadFile('index.html');
+  win.removeMenu();
+  win.loadFile('cache/client/index.html');
+  win.webContents.openDevTools();
 };
 
 app.whenReady().then((): void => {
